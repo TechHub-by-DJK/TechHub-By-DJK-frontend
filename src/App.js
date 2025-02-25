dbd31bf27ca14a257584cae64889f66b815d1ef8
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2025-02-25 21:47:31 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-blue-500'> code  with DJK</h1>
+    </div>
+  );
+}
+
+export default App;
