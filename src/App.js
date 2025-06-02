3dd23afd3c8f7a3c38abee222e068f8a9e52a11a
Janith Prabash Kashmira R.K. <138957977+janithprabashrk@users.noreply.github.com>
2025-06-02 21:59:55 +0530

    [TechHub 000] FEAT : App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { ThemeProvider } from '@emotion/react';
 import './App.css';
 import Navbar from './component/Navbar/Navbar';
+import { darkTheme } from './Theme/DarkTheme';
+import { CssBaseline } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Navbar/>
+      </ThemeProvider>
     </div>
   );
 }
