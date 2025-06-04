226ab1bfb168b405a5fa354f8e9d0d5743b35638
Janith Prabash Kashmira R.K. <138957977+janithprabashrk@users.noreply.github.com>
2025-06-04 18:20:27 +0530

    [TechHub 001] CREATE : Create slider

---
+++ src/component/Home/MultiItemCarousal.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
+import { topPc } from './TopPc';
+import CarousalItem from './CarousalItem';
+
+const MultiItemCarousal = () => {
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    };
+
+  return (
+    <div>
+        <Slider {...settings}>
+            {topPc.map((item) =>(
+                <CarousalItem image={item.image} title={item.title}/> ))}
+        </Slider>
+    </div>
+  )
+}
+
+export default MultiItemCarousal
