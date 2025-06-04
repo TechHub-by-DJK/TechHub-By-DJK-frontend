68cca88291c7e4bc03816956b2882b933e4de6d3
Janith Prabash Kashmira R.K. <138957977+janithprabashrk@users.noreply.github.com>
2025-06-04 18:21:48 +0530

    [TechHub 001] FEAT : Add Home for app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './component/Navbar/Navbar';
 import { darkTheme } from './Theme/DarkTheme';
 import { CssBaseline } from '@mui/material';
+import Home from './component/Home/Home';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
         <Navbar/>
+        <Home />
       </ThemeProvider>
     </div>
   );
