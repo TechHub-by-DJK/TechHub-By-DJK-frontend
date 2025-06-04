c275eabc494cc9bed6d37ab6536967e5270bc8e4
Janith Prabash Kashmira R.K. <138957977+janithprabashrk@users.noreply.github.com>
2025-06-04 18:20:54 +0530

    [TechHub 001] FEAT : Data for carousal

---
+++ src/component/Home/TopPc.js
@@ -0,0 +1,47 @@
+export const topPc = [
+    {
+        image: "https://images.unsplash.com/photo-1593642532973-d31b6557fa68?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+        title: "Asus ROG Zephyrus G14",
+    },
+    {
+        image: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?auto=format&fit=crop&w=800&q=80",
+        title: "Apple MacBook Pro",
+    },
+    {
+        image: "https://images.unsplash.com/photo-1519389950473-47ba0277781c?auto=format&fit=crop&w=800&q=80",
+        title: "Dell XPS 13 Business Laptop",
+    },
+    {
+        image: "https://images.unsplash.com/photo-1518770660439-4636190af475?auto=format&fit=crop&w=800&q=80",
+        title: "Corsair Vengeance RAM 16GB",
+    },
+    {
+        image: "https://images.unsplash.com/photo-1519125323398-675f0ddb6308?auto=format&fit=crop&w=800&q=80",
+        title: "NVIDIA GeForce RTX 3080",
+    },
+    {
+        image: "https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=800&q=80",
+        title: "Logitech Gaming Mouse",
+    },
+    {
+        image: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?auto=format&fit=crop&w=800&q=80",
+        title: "Mechanical Gaming Keyboard",
+    },
+    {
+        image: "https://images.unsplash.com/photo-1519985176271-adb1088fa94c?auto=format&fit=crop&w=800&q=80",
+        title: "Samsung SSD 1TB",
+    },
+    {
+        image: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?auto=format&fit=crop&w=800&q=80",
+        title: "MacBook Air M2",
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2017/08/01/13/36/computer-2565478_1280.jpg",
+        title: "Business Desktop PC",
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2024/05/22/22/33/interior-design-8781907_1280.png",
+        title: "Gaming Computer Setup",
+    }
+];
+    
