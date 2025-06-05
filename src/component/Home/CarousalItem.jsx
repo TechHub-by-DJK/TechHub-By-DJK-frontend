88b761b55085970719b5f2ffa78a9ddd64ff4050
Janith Prabash Kashmira R.K. <138957977+janithprabashrk@users.noreply.github.com>
2025-06-05 12:41:37 +0530

    [TechHub 001] FEAT: Update sizes for responsiveness

--- src/component/Home/CarousalItem.jsx
+++ src/component/Home/CarousalItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CarousalItem = ({image, title}) => {
   return (
     <div className='flex flex-col justify-center items-center'>
-        <img className='w-[10rem] h-[10rem] lg:h-[14rem] lg:w-[14rem] rounded-full object-cover object-center' src={image} alt=''>
+        <img className='w-[6rem] h-[6rem] lg:h-[14rem] lg:w-[14rem] rounded-full object-cover object-center' src={image} alt=''>
         </img>
         <span className='py-5 font-semibold text-xl text-gray-400'>{title}</span>
     </div>
