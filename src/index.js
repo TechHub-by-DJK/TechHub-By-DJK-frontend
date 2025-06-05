e74f8d101978116491ac6b57acb81d0d564b90f2
Janith Prabash Kashmira R.K. <138957977+janithprabashrk@users.noreply.github.com>
2025-06-05 22:44:19 +0530

    [TechHub 000] CREATE : Update system font outfit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import Outfit font weights
+import '@fontsource/outfit/300.css';
+import '@fontsource/outfit/400.css';
+import '@fontsource/outfit/500.css';
+import '@fontsource/outfit/600.css';
+import '@fontsource/outfit/700.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
